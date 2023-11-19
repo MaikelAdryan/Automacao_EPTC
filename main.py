5e5db34a78cf93b7c22b608e6bd0f9f9d9bac645
adryanmaikel <adryanmaikel@gmail.com>
2023-11-19 10:55:14 -0300

    adicionando para baixar os excels

--- main.py
+++ main.py
@@ -1,36 +1,55 @@
-import PySimpleGUI as sg
-from browser import start_firefox, close_firefox
+from PySimpleGUI import (
+  Window, theme, WINDOW_CLOSED, Push, Button, Text, HSep, VSep)
+  
+from browser import start_firefox, close_firefox, download_excel
 
-browser = None
-
-sg.theme('Python')
-LAYOUT = [
-  # [sg.Text('')],
-  [sg.Push(),
-   sg.Button('Abrir Firefox', disabled=False),
-   sg.Button('Fechar Firefox', disabled=False),
-   sg.Push()],
-  [sg.Text(text='', key='response firefox', visible=False)],
-  [sg.HSeparator()]
+theme('Gray Gray Gray')
+browser, LAYOUT = None, [
+  [Text('Pegar reclamações da EPTC'), Push()],
+  [
+    Push(),
+    Button('Abrir Firefox', size=(18, 1), disabled=False),
+    Button('Fechar Firefox', size=(18, 1), disabled=True),
+    Push()
+  ],
+  [Text(text='', key='response firefox', visible=False)],
+  [HSep()],
+  [Text('Baixar as planilhas.'), Push()],
+  [
+    Button('LOTE 1', size=(7, 1), disabled=True),
+    Button('LOTE 2', size=(7, 1), disabled=True)
+  ],
+  [],
 ]
-WINDOW = sg.Window('Automação EPTC', LAYOUT)
+WINDOW = Window('EPTC', LAYOUT)
 
 while True:
   events, values = WINDOW.read()
   
   if events == 'Abrir Firefox':
     browser = start_firefox()
+    WINDOW['Abrir Firefox'].update(disabled=True)
     WINDOW['Fechar Firefox'].update(disabled=False)
-    
+    WINDOW['LOTE 1'].update(disabled=False)
+    WINDOW['LOTE 2'].update(disabled=False)
   
   if events == 'Fechar Firefox':
     response = close_firefox(browser)
-    print(response)
+    browser = None
     WINDOW['response firefox'].update(response)
     WINDOW['response firefox'].update(visible=True)
+    WINDOW['Abrir Firefox'].update(disabled=False)
+    WINDOW['Fechar Firefox'].update(disabled=True)
+    WINDOW['LOTE 1'].update(disabled=False)
+    WINDOW['LOTE 2'].update(disabled=False)
+    
+  if events == 'LOTE 1':
+    download_excel(browser, 1)
+  
+  if events == 'LOTE 2':
+    download_excel(browser, 2)
 
-
-  if events == sg.WINDOW_CLOSED:
+  if events == WINDOW_CLOSED:
     break
 
 WINDOW.close()
