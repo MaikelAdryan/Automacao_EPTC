20bc92bc4a62e8111985086cd7b97b90f611a853
adryanmaikel <adryanmaikel@gmail.com>
2023-11-19 10:48:45 -0300

    criando função para baixar excel

--- browser.py
+++ browser.py
@@ -1,5 +1,8 @@
 from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from login import login
 
+URL_EPTC = 'https://156poa.procempa.com.br'
 FIREFOX = Firefox
 
 def start_firefox():
@@ -10,9 +13,27 @@ def start_firefox():
   """
   try:
     BROWSER = FIREFOX()
+    BROWSER.get(URL_EPTC)
+    login()
     return BROWSER
   except:
     return 'Falha ao abrir o firefox.'
+
+
+def download_excel(browser: Firefox, lote: int):
+  LOTE_1, LOTE_2 = '999901074', '999901075'
+  URL_FILA = f'{URL_EPTC}/sistemas/156/fila/servicos_local.php?'
+  URL_EXCEL_LOTE_1 = f'{URL_FILA}cod_local_destino={LOTE_1}'
+  URL_EXCEL_LOTE_2 = f'{URL_FILA}cod_local_destino={LOTE_2}'
+  XPATH_EXCEL = '//html/body/table/tbody/tr[1]/th[2]/a'
+  if lote == 1:
+    browser.get(URL_EXCEL_LOTE_1)
+    browser.find_element(By.XPATH, XPATH_EXCEL).click()
+  elif lote == 2:
+    browser.get(URL_EXCEL_LOTE_2)
+    browser.find_element(By.XPATH, XPATH_EXCEL).click()
+  else:
+    return 'Lote inválido!'
 
 
 def close_firefox(browser: Firefox):
