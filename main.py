3059f44b65229356c63a462d1c458b81edf415ee
adryanmaikel <adryanmaikel@gmail.com>
2023-11-19 10:52:00 -0300

    adicionando para baixar os excels

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from PySimpleGUI import (
-  Window, theme, WINDOW_CLOSED, Push, Button, Text, HSeparator)
+  Window, theme, WINDOW_CLOSED, Push, Button, Text, HSep, VSep)
   
-from browser import start_firefox, close_firefox
-
-SIZE_BUTTONS = (18, 1)
+from browser import start_firefox, close_firefox, download_excel
 
 theme('Gray Gray Gray')
 browser, LAYOUT = None, [
   [Text('Pegar reclamações da EPTC'), Push()],
-  [Push(),
-   Button('Abrir Firefox', size=SIZE_BUTTONS, disabled=False),
-   Button('Fechar Firefox', size=SIZE_BUTTONS, disabled=True),
-   Push()],
+  [
+    Push(),
+    Button('Abrir Firefox', size=(18, 1), disabled=False),
+    Button('Fechar Firefox', size=(18, 1), disabled=True),
+    Push()
+  ],
   [Text(text='', key='response firefox', visible=False)],
-  [HSeparator()],
+  [HSep()],
   [Text('Baixar as planilhas.'), Push()],
-  [Button('LOTE 1'), Button('LOTE 2')],
+  [
+    Button('LOTE 1', size=(7, 1), disabled=True),
+    Button('LOTE 2', size=(7, 1), disabled=True)
+  ],
+  [],
 ]
 WINDOW = Window('EPTC', LAYOUT)
 
@@ -26,6 +30,8 @@ while True:
     browser = start_firefox()
     WINDOW['Abrir Firefox'].update(disabled=True)
     WINDOW['Fechar Firefox'].update(disabled=False)
+    WINDOW['LOTE 1'].update(disabled=False)
+    WINDOW['LOTE 2'].update(disabled=False)
   
   if events == 'Fechar Firefox':
     response = close_firefox(browser)
@@ -34,8 +40,14 @@ while True:
     WINDOW['response firefox'].update(visible=True)
     WINDOW['Abrir Firefox'].update(disabled=False)
     WINDOW['Fechar Firefox'].update(disabled=True)
+    WINDOW['LOTE 1'].update(disabled=False)
+    WINDOW['LOTE 2'].update(disabled=False)
     
-
+  if events == 'LOTE 1':
+    download_excel(browser, 1)
+  
+  if events == 'LOTE 2':
+    download_excel(browser, 2)
 
   if events == WINDOW_CLOSED:
     break
