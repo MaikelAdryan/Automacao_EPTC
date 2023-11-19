4ddcbc948599409fa4c30b7bdb899c3f140dae78
adryanmaikel <adryanmaikel@gmail.com>
2023-11-18 21:06:58 -0300

    melhorando layout

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
-import PySimpleGUI as sg
+from PySimpleGUI import (
+  Window, theme, WINDOW_CLOSED, Push, Button, Text, HSeparator)
+  
 from browser import start_firefox, close_firefox
 
-browser = None
+SIZE_BUTTONS = (18, 1)
 
-sg.theme('Python')
-LAYOUT = [
-  # [sg.Text('')],
-  [sg.Push(),
-   sg.Button('Abrir Firefox', disabled=False),
-   sg.Button('Fechar Firefox', disabled=False),
-   sg.Push()],
-  [sg.Text(text='', key='response firefox', visible=False)],
-  [sg.HSeparator()]
+theme('Gray Gray Gray')
+browser, LAYOUT = None, [
+  [Text('Pegar reclamações da EPTC'), Push()],
+  [Push(),
+   Button('Abrir Firefox', size=SIZE_BUTTONS, disabled=False),
+   Button('Fechar Firefox', size=SIZE_BUTTONS, disabled=True),
+   Push()],
+  [Text(text='', key='response firefox', visible=False)],
+  [HSeparator()],
+  [Text('Baixar as planilhas.'), Push()],
+  [Button('LOTE 1'), Button('LOTE 2')],
 ]
-WINDOW = sg.Window('Automação EPTC', LAYOUT)
+WINDOW = Window('EPTC', LAYOUT)
 
 while True:
   events, values = WINDOW.read()
   
   if events == 'Abrir Firefox':
     browser = start_firefox()
+    WINDOW['Abrir Firefox'].update(disabled=True)
     WINDOW['Fechar Firefox'].update(disabled=False)
-    
   
   if events == 'Fechar Firefox':
     response = close_firefox(browser)
-    print(response)
+    browser = None
     WINDOW['response firefox'].update(response)
     WINDOW['response firefox'].update(visible=True)
+    WINDOW['Abrir Firefox'].update(disabled=False)
+    WINDOW['Fechar Firefox'].update(disabled=True)
+    
 
 
-  if events == sg.WINDOW_CLOSED:
+  if events == WINDOW_CLOSED:
     break
 
 WINDOW.close()
