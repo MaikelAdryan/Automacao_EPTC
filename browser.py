7302d395efae61b48b26314bbb0bcebc34d9990d
NGS-Adryan <ngs.adryan@gmail.com>
2023-11-22 10:50:31 -0300

    Criando função para ir até a url de downloads
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

--- browser.py
+++ browser.py
@@ -12,7 +12,7 @@ XPATH_BUTTON_DOWNLOAD_EXCEL = '//html/body/table/tbody/tr[1]/th[2]/a'
 FIREFOX = Firefox
 
 def start_firefox():
-  """Função para abrir o navegador firefox
+  """Função para abrir o navegador firefox e ir até o site da EPTC
 
   Returns:
     webdriver.Firefox: Retorna o navegador firefox
@@ -25,20 +25,26 @@ def start_firefox():
     return 'Falha ao abrir o firefox.'
 
 
+def goto_url_download(URL_LOTE: str):
+	BROWSER = start_firefox()
+	BROWSER.get(URL_LOTE)
+	BROWSER.find_element(By.XPATH, XPATH_BUTTON_DOWNLOAD_EXCEL).click()
+	close_firefox(BROWSER)
+
+
 def download_excel(lote: int):
-  BROWSER = start_firefox()
-  clear_dir_download()
-  
-  if lote == 1:
-    BROWSER.get(URL_EXCEL_LOTE_1)
-    BROWSER.find_element(By.XPATH, XPATH_BUTTON_DOWNLOAD_EXCEL).click()
-    close_firefox(BROWSER)
-  elif lote == 2:
-    BROWSER.get(URL_EXCEL_LOTE_2)
-    BROWSER.find_element(By.XPATH, XPATH_BUTTON_DOWNLOAD_EXCEL).click()
-    close_firefox(BROWSER)
-  else:
-    return 'Lote inválido!'
+	clear_dir_download()
+
+	match lote:
+		case 1:
+			goto_url_download(URL_EXCEL_LOTE_1)
+
+		case 2:
+			goto_url_download(URL_EXCEL_LOTE_2)
+
+		case _ :
+			return 'Lote inválido!'
+
 
 
 def close_firefox(browser: Firefox):
@@ -60,4 +66,5 @@ def close_firefox(browser: Firefox):
 if __name__ == '__main__':
   # browser = start_firefox()
   # print(close_firefox(browser))
-	print(URL_EXCEL_LOTE_1)
+	print(download_excel(2))
+	pass
