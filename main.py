dc087389452581028f239d87a8a454144dc49d36
AdryanMaikel <adryanmaikel@gmail.com>
2023-11-26 09:47:49 -0300

    finalizando projeto e refatorando código!
    Co-authored-by: NGS-Adryan <ngs.adryan@gmail.com>

--- main.py
+++ main.py
@@ -1,72 +1,73 @@
 # -*- coding: utf-8 -*-
 
 from PySimpleGUI import (
-	Window, theme, WINDOW_CLOSED, Push, Button, Text, HSep)
-from browser import download_excel
+  Window, theme, WINDOW_CLOSED, Push, Button, Text, HSep)
+from browser import download_excel, get_informations_from_reclamation
 from excel import read_excel
 
 theme('Topanga')
 browser, LAYOUT = None, [
-	[Text('Baixar as planilhas.', font=(12))],
-	[
-		Push(),
-		Button('LOTE 1', key='download lote 1', size=(7, 1), disabled=False),
-		Button('LOTE 2', key='download lote 2', size=(7, 1), disabled=False),
-		Button('AMBOS', key='download lotes', size=(7, 1), disabled=False),
-		Push()
-	],
-	[Text('', key='response donwload', visible=False)],
-	
-	[HSep()],
-	
-	[Text('Ler excels', font=(12)), Push()],
-	[
-		Push(),
-		Button('LOTE 1', key='extract lote 1', size=(7, 1), disabled=False),
-		Button('LOTE 2', key='extract lote 2', size=(7, 1), disabled=False),
-		Button('AMBOS', key='extract lotes', size=(7, 1), disabled=False),
-		Push()
-	],
-	[Text('', key='response readed', visible=False)],
-	[HSep()],
+  [Text('Baixar as planilhas.', font=(12))],
+  [
+    Push(),
+    Button('LOTE 1', key='download lote 1', size=(7, 1), disabled=False),
+    Button('LOTE 2', key='download lote 2', size=(7, 1), disabled=False),
+    Button('AMBOS', key='download lotes', size=(7, 1), disabled=False),
+    Push()
+  ],
+  [Text('', key='response donwload', visible=False)],
+
+  [HSep()],
+
+  [Text('Ler excels', font=(12)), Push()],
+  [
+    Push(),
+    Button('LOTE 1', key='extract lote 1', size=(7, 1), disabled=False),
+    Button('LOTE 2', key='extract lote 2', size=(7, 1), disabled=False),
+    Button('EXTRAIR', key='extract lotes', size=(7, 1), disabled=False),
+    Push()
+  ],
+  [Text('', key='response readed', visible=False)],
+
+  [HSep()],
 ]
 WINDOW = Window('EPTC', LAYOUT)
 
 def add_response(key: str, new_text: str, visible: bool, color: str):
-	WINDOW[key].update(visible=visible)
-	WINDOW[key].update(text_color=color)
-	WINDOW[key].update(new_text)
+  WINDOW[key].update(visible=visible)
+  WINDOW[key].update(text_color=color)
+  WINDOW[key].update(new_text)
 
 
 while True:
-	events, values = WINDOW.read()
+  events, values = WINDOW.read()
 
-	if events == 'download lote 1':
-		color, response = download_excel(1)
-		add_response('response donwload', response, True, color)
-		
-	if events == 'download lote 2':
-		color, response = download_excel(2)
-		add_response('response donwload', response, True, color)
+  if events == 'download lote 1':
+    color, response = download_excel(1)
+    add_response('response donwload', response, True, color)
 
-	if events == 'download lotes':
-		for i in range(1, 3):
-			color, response = download_excel(i)
-		response = 'Excels LOTE 1 e 2 atualizados com sucesso!'
-		add_response('response donwload', response, True, color)
-	
-	if events == 'extract lote 1':
-		color, response = read_excel('LOTE_1.xls')
-		add_response('response readed', response, True, color)
-	
-	if events == 'extract lote 2':
-		color, response = read_excel('LOTE_2.xls')
-		add_response('response readed', response, True, color)
-	
-	if events == 'extract lotes':
-		pass
+  if events == 'download lote 2':
+    color, response = download_excel(2)
+    add_response('response donwload', response, True, color)
 
-	if events == WINDOW_CLOSED:
-		break
+  if events == 'download lotes':
+    for i in range(1, 3):
+      color, response = download_excel(i)
+      response = 'Excels LOTE 1 e 2 atualizados com sucesso!'
+      add_response('response donwload', response, True, color)
+
+  if events == 'extract lote 1':
+    color, response = read_excel('LOTE_1.xls')
+    add_response('response readed', response, True, color)
+
+  if events == 'extract lote 2':
+    color, response = read_excel('LOTE_2.xls')
+    add_response('response readed', response, True, color)
+
+  if events == 'extract lotes':
+    get_informations_from_reclamation()
+
+  if events == WINDOW_CLOSED:
+    break
 
 WINDOW.close()
