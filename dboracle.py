a3565a8c8d1fe1f0c6c6bf5a3bc10d8c8c61326f
NGS-Adryan <ngs.adryan@gmail.com>
2023-11-29 11:57:59 -0300

    Configurando e pegandos todos protocolos da tabela
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

--- dboracle.py
+++ dboracle.py
@@ -1,8 +1,38 @@
-from login_dboracle import UN, CS, PW
 import oracledb
+import configparser
 
-with oracledb.connect(user=UN, password=PW, dsn=CS) as connection:
-  with connection.cursor() as cursor:
-    sql = """select sysdate from dual"""
-  for response in cursor.execute(sql):
-    print(response)
+FILE_CONFIG = 'dboracle.ini'
+CONFIG = configparser.ConfigParser()
+CONFIG.read(FILE_CONFIG)
+DB_CONFIG = CONFIG['database']
+
+ORACLE_CLIENT = DB_CONFIG['client_oracle']
+oracledb.init_oracle_client(lib_dir=ORACLE_CLIENT)
+
+ORACLE_USER, ORACLE_PASS = DB_CONFIG['user'], DB_CONFIG['password']
+ORACLE_HOST, ORACLE_PORT = DB_CONFIG['host'], DB_CONFIG['port']
+ORACLE_SERVICE = DB_CONFIG['service']
+DNS = oracledb.makedsn(
+  host=ORACLE_HOST, port=ORACLE_PORT, service_name=ORACLE_SERVICE)
+
+def get_protocols():
+  """Pega todos os protocolos do banco de dados.
+
+  Returns:
+    PROTOCOLS (list): Lista contendo todos os protocolos
+  """
+  PROTOCOLS = []
+  with oracledb.connect(
+    user=ORACLE_USER, password=ORACLE_PASS, dsn=DNS) as CONNECTION:
+    with CONNECTION.cursor() as CURSOR:
+      QUERY = f"""select PROTOCOLO from {DB_CONFIG['table']}"""
+      CURSOR.execute(QUERY)
+      db_protocols = CURSOR.fetchall()
+      for protocol in db_protocols:
+        PROTOCOLS.append(protocol[0])
+  return PROTOCOLS
+
+
+if __name__ == '__main__':
+  print(get_protocols())
+  pass
