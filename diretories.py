ea68add0d1c7d87106e76c67b80182d6eb4d514d
NGS-Adryan <ngs.adryan@gmail.com>
2023-12-05 14:58:28 -0300

    criando função para limpar arquivos
    Co-authored-by: AdryanMaikel <adryanmaikel@gmail.com>

--- diretories.py
+++ diretories.py
@@ -8,3 +8,10 @@ DIR_DOWNLOAD = f'{USER_PATH}/Downloads'
 
 DIR_NGS = f'{DIR_ACTUAL.split("/")[0]}/NGS/SERVICES/'
 DIR_LOGS = f'{DIR_NGS}logs/'
+
+def clear_file(file: str):
+  try:
+    with open(f'{DIR_TEMP}{file}.json', 'w', encoding='utf-8') as FILE:
+      FILE.write('')
+  except:
+    print('An exception occurred')
